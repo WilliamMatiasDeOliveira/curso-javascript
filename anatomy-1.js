55e2351ad1b77270da0320a8fe4f23ccba439c0b
William Matis de Oliveira <williammatiasjau@hotmail.com>
2025-04-12 21:57:57 -0300

    aula 01

---
+++ anatomy-1.js
@@ -0,0 +1,26 @@
+function sayHello(){
+    console.log('Hello !')
+}
+
+sayHello()
+
+function sayHelloTo(name){
+    console.log(`Hello ! ${name}`)
+}
+
+sayHelloTo("João")
+
+function returnHi(){
+    return "Hi !"
+}
+
+const greeting = returnHi()
+console.log(greeting)
+console.log(returnHi())
+
+function returnHiTo(name){
+    return `hi ${name}`
+}
+
+console.log(returnHiTo("Jhon"))
+
