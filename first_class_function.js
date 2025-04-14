3a3ef46e6d0fa45602bed52e37d0072da6f44247
William Matis de Oliveira <williammatiasjau@hotmail.com>
2025-04-14 18:15:55 -0300

    first class function

---
+++ first_class_function.js
@@ -0,0 +1,16 @@
+// firs class function
+const add = function(a, b){
+    return a + b
+}
+
+const sub = (a, b) => {
+    return a - b
+}
+
+const mult = (a, b) => {
+    return a * b
+}
+
+const div = (a, b) => a / b
+
+console.log(add(10, 20))
