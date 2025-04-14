a3fafdfaf41023312caeb4d4b695f54d9d00f8a6
William Matis de Oliveira <williammatiasjau@hotmail.com>
2025-04-14 18:24:47 -0300

    function call other function

---
+++ higher_order_function1.js
@@ -0,0 +1,18 @@
+// function call oder function
+
+function run(fn){
+    return fn()
+}
+
+function sayHello(){
+    console.log("Hello !")
+}
+
+run(sayHello)
+
+run(function(){
+    console.log("run !")
+})
+
+const result = run(Math.random)
+console.log(result)
