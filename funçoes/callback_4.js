1c64c5f43a219f25c9cbe5aaf1562a6490545724
william matias de oliveira <williammatiasjau@hotmail.com>
2025-06-19 15:18:18 -0300

    Função Filter#03

--- funçoes/callback_4.js
+++ funçoes/callback_4.js
@@ -12,7 +12,25 @@ const qtdeMaiorQueZero = item => item.qtde > 0
 //const qtdeMuitoGrande = item => item.qtde >= 1000
 
 const nomeItensValido = carrinho
-    .filter(qtdeMaiorIgualAZero)
+    .filter(qtdeMaiorQueZero)
         .map(getNome)
 
 console.log(nomeItensValido)
+
+Array.prototype.meuFilter = function(fn) {
+    const novoArray = []
+
+    for(let i = 0; i < this.length; i++) {
+        if(fn(this[i], i, this)) {
+            novoArray.push(this[i])
+        }
+    }
+
+    return novoArray
+}
+
+const nomeItensValido2 = carrinho
+    .meuFilter(qtdeMaiorQueZero)
+        .map(getNome)
+
+console.log(nomeItensValido2)
